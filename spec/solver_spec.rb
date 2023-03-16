98bb7be18bf2226228ebbea2d25d64ba9bc47c64
Glenda Diaz <glendadiazah@outlook.com>
2023-03-16 12:31:16 -0600

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,5 +8,17 @@ describe Solver do
     it 'Create an instance of Solver class' do
       problem.should be_instance_of Solver
     end
+
+    it 'returns the factorial of n' do
+      expect(problem.factorial(5)).to eq 120
+    end
+
+    it 'raises exception' do 
+      expect { problem.factorial(-5) }.to raise_exception(RuntimeError)
+    end
+
+    it 'equals 1' do 
+      expect(problem.factorial(0)).to eq 1
+    end
   end
 end
