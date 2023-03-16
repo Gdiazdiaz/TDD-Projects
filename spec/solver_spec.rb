84955b7d4eed4e0b2360639c1ae43978d6dd5995
Glenda Diaz <glendadiazah@outlook.com>
2023-03-16 12:47:37 -0600

    Add test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -23,5 +23,21 @@ describe Solver do
     it 'returns the reverse string' do
       expect(problem.reverse('hello')).to eql('olleh')
     end
+    
+    it 'returns fizzbuzz' do
+      expect(problem.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+
+    it 'returns fizz' do
+      expect(problem.fizzbuzz(9)).to eql 'fizz'
+    end
+
+    it 'returns buzz' do
+      expect(problem.fizzbuzz(50)).to eql 'buzz'
+    end
+
+    it 'returns same number as string' do
+      expect(problem.fizzbuzz(7)).to eql '7'
+    end
   end
 end
