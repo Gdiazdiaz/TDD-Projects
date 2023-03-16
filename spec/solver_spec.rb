f42f32a83fee87214b20a68ae6ad9a869ebc45a9
Giovanni Juliao <gjuliao32@gmail.com>
2023-03-16 10:56:55 -0500

    Added reverse method failing test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,5 +7,9 @@ describe Solver do
     it 'Create an instance of Solver class' do
       problem.should be_instance_of Solver
     end
+
+    it 'returns the reverse string' do
+      expect(problem).to receive(:reverse).with('hello').and_return('olleh')
+    end
   end
 end
